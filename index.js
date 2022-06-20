ab727560935258538f0992f04d1116e797dfd371
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-20 17:40:55 +0530

    test

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// let a=1;
+//   let b=1;
+//   for(let i=1; i<=0; i++){
+//     conole.log(a);
+//     let  c=a+b;
+//     a=b;
+//     b=c;
+//   }
+
+
+// let temp=parseInt(process.argv[2])
+// let celsius = (temp-32)*5/9;
+//   console.log (celsius);
+
+
+
+// let number_of_days = parseInt(process.argv[2])
+// if(number_of_days>=0 && number_of_days<=5){
+//     console.log (0*number_of_days);
+//   }
+//    else if(number_of_days>=6 && number_of_days<=10){
+//     console.log (3*number_of_days);
+//   }
+//    else if(number_of_days>=11 && number_of_days<=15){
+//     console.log (4*number_of_days);
+//   }
+//    else{
+//     console.log (5*number_of_days);
+//   }
+
